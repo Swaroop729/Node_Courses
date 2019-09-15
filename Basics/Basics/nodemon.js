2ea9d965cd0f0f0ab4d52d472fe28a3d54bc6792
Swaroop <swaroop1011@gmail.com>
2019-09-15 22:29:44 +0530

    instlng nodemon globally instd node due to watcher

---
+++ Basics/Basics/nodemon.js
@@ -0,0 +1,15 @@
+/*Nodemon : 
+While using the node we need to go back each time run the node command for the file in the terminal 
+instead of that we can add a cmd terminal utility as Nodemon and we can use it.
+To install nodemon 
+Npm install nodemon –g 
+
+While using generally we use the scripts running with node then node watcher is not enabled
+
+but while using the nodemon the watcher is enabled and once you change the sourcefile and save it
+it instantly runs and shows the output
+
+Usage using node : node filename.js
+Usage using nodemon : nodemon filename.js
+
+*/
